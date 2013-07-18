e60cbbfce3634cd22bbc211cbcd4b79746bc8f72
Gian Giovani <wejick@gmail.com>
2013-07-18 14:20:26 +0700

    first commit

---
+++ ppStream/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ppStream
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listFile = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listFile
+            // 
+            this.listFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listFile.FormattingEnabled = true;
+            this.listFile.Location = new System.Drawing.Point(0, 0);
+            this.listFile.Name = "listFile";
+            this.listFile.Size = new System.Drawing.Size(284, 262);
+            this.listFile.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.listFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listFile;
+    }
+}
+
